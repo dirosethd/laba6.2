dfa7fc3b4dacb9c244725ebae0b7dfa12a4b8031
dirose <dirose@DESKTOP-PQA70RD>
2024-12-27 03:41:15 +0200

    Добавьте файлы проекта.

---
+++ Matrix/Class1.cs
@@ -0,0 +1,30 @@
+﻿namespace Matrix
+{
+    public class Class1
+    {
+        public class Matrix
+        {
+            private int[,] _elements;
+
+            public Matrix(int[,] elements)
+            {
+                if (elements.GetLength(0) != elements.GetLength(1))
+                {
+                    throw new ArgumentException("Матрица должна быть квадратной.");
+                }
+                _elements = elements;
+            }
+
+            public int CalculateDiagonalProduct()
+            {
+                int product = 1;
+                int size = _elements.GetLength(0);
+                for (int i = 0; i < size; i++)
+                {
+                    product *= _elements[i, i];
+                }
+                return product;
+            }
+        }
+    }
+}
---
+++ laba6.2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n, m;
+
+        Console.WriteLine("Введите количество строк (n): ");
+        n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите количество столбцов (m): ");
+        m = int.Parse(Console.ReadLine());
+
+        if (n != m)
+        {
+            Console.WriteLine("Матрица должна быть квадратной (n = m).");
+            return;
+        }
+        int[,] matrix = new int[n, m];
+        Console.WriteLine("Введите элементы матрицы (целые числа): ");
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                Console.Write($"Элемент [{i + 1},{j + 1}]: ");
+                matrix[i, j] = int.Parse(Console.ReadLine());
+            }
+        }
+        int product = 1;
+        for (int i = 0; i < n; i++)
+        {
+            product *= matrix[i, i];
+        }
+        Console.WriteLine($"Произведение элементов главной диагонали матрицы: {product}");
+    }
+}
